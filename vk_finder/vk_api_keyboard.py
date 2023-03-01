18008dfe5c85d85255576847cfdd5b50b72e5ac2
Serhii Borodin <69416078+serj-goa@users.noreply.github.com>
2023-03-01 18:14:18 +0300

    Create keyboard

---
+++ vk_finder/vk_api_keyboard.py
@@ -0,0 +1,19 @@
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+
+def create_keyboard(colors: tuple[tuple]) -> VkKeyboard:
+    """
+    Creates a keyboard in the user chatbot.
+    """
+    keyboard_colors = {
+        'blue': VkKeyboardColor.PRIMARY,
+        'white': VkKeyboardColor.SECONDARY,
+        'green': VkKeyboardColor.POSITIVE,
+        'red': VkKeyboardColor.NEGATIVE,
+    }
+    keyboard = VkKeyboard(one_time=True)
+
+    for kbrd_label, kbrd_color in colors:
+        keyboard.add_button(label=kbrd_label, color=keyboard_colors[kbrd_color])
+
+    return keyboard
